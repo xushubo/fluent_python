11541ffa63de0389fb722e3b45f5db4c0bd2b127
xushubo <tmac523@163.com>
2017-10-05 09:13:52 +0800

    Delete 4_6_UnicodeDecodeError.py

--- 4_6_UnicodeDecodeError.py
+++
@@ -1,9 +0,0 @@
-octets = b'Montr\xe9al'
-print(octets.decode('cp1252'))
-print(octets.decode('iso8859_7'))
-print(octets.decode('koi8_r'))
-try:
-    octets.decode('utf-8')
-except UnicodeDecodeError:
-    print('UnicodeDecodeError')
-print()
