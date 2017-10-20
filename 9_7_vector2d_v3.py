17333911d108ab54503368bdd37b1937e5d0fec1
xushubo <tmac523@163.com>
2017-10-20 16:56:13 +0800

    updated 2018-10-20

--- 9_7_vector2d_v3.py
+++ 9_7_vector2d_v3.py
@@ -5,8 +5,16 @@ class Vector2d():
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -19,11 +27,14 @@ class Vector2d():
         return str(tuple(self))
 
     def __bytes__(self):
-        return (bytes([ord(self.typecode)]) + 
+        return (bytes([ord(self.typecode)]) +
                 bytes(array(self.typecode, self)))
 
     def __eq__(self, other):
         return tuple(self) == tuple(other)
+    
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
 
     def __abs__(self):
         return math.hypot(self.x, self.y)
@@ -31,14 +42,14 @@ class Vector2d():
     def __bool__(self):
         return bool(abs(self))
 
-    # def __format__(self, fmt_spec=''):
-    #     components = (format(c, fmt_spec) for c in self)
-    #     return '({}, {})'.format(*components)
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
     def __format__(self, fmt_spec=''):
         if fmt_spec.endswith('p'):
             fmt_spec = fmt_spec[:-1]
             coords = (abs(self), self.angle())
-            outer_fmt = '<{}, {}>'
+            outer_fmt = '<{}. {}>'
         else:
             coords = self
             outer_fmt = '({}, {})'
@@ -48,25 +59,30 @@ class Vector2d():
     @classmethod
     def frombytes(cls, octets):
         typecode = chr(octets[0])
-        memv = memoryview(octets[1:]).cast(typecode)
+        memv = memoryview(octes[1:]).cast(typecode)
         return cls(*memv)
 
-    def angle(self):
-        return math.atan2(self.y, self.x)
-
 v1 = Vector2d(3, 4)
+v2 = Vector2d(3.1, 4.2)
+print(hash(v1), hash(v2))
+print(set([v1, v2]))
 print(v1.x, v1.y)
-x, y = v1
-print(x, y)
-print(repr(v1))
-v1_clone = eval(repr(v1))
-print(v1 == v1_clone)
-print(v1)
-octets = bytes(v1)
-print(octets)
-print(abs(v1))
-print(bool(v1), bool(Vector2d(0, 0)))
 
-print(format(Vector2d(1, 1), 'p'))
-print(format(Vector2d(1, 1), '3ep'))
-print(format(Vector2d(1, 1), '0.5fp'))
+# 9-13
+v1 = Vector2d(1.1, 2.2)
+dumpd = bytes(v1)
+print(dumpd)
+print(len(dumpd))
+v1.typecode = 'f'
+dumpf = bytes(v1)
+print(dumpf)
+print(len(dumpf))
+print(Vector2d.typecode)
+
+# 9-14
+class ShortVector2d(Vector2d):
+    typecode = 'f'
+
+sv = ShortVector2d(1/11, 1/27)
+print(sv)
+print(len(bytes(sv)))
