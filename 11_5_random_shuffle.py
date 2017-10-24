09431a5d61cccbb66b98e922f98321967a08d239
xushubo <tmac523@163.com>
2017-10-24 17:08:23 +0800

    updated 2017-10-24

---
+++ 11_5_random_shuffle.py
@@ -0,0 +1,28 @@
+from random import shuffle
+l = list(range(10))
+shuffle(l)
+print(l)
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+def set_card(deck, position, card):
+    deck._cards[position] = card
+FrenchDeck.__setitem__ = set_card
+deck = FrenchDeck()
+shuffle(deck)
+print(deck[:5])
