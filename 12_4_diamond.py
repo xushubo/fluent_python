21112ac1395cde2a059486fd8c1b21255e19816f
xushubo <tmac523@163.com>
2017-10-31 17:31:01 +0800

    updated 2017-10-31

---
+++ 12_4_diamond.py
@@ -0,0 +1,34 @@
+class A:
+    def ping(self):
+        print('ping:', self)
+
+class B(A):
+    def pong(self):
+        print('pong:', self)
+
+class C(A):
+    def pong(self):
+        print('PONG:', self)
+
+class D(B, C):
+    def ping(self):
+        super().ping()
+        # A.ping(self)
+        print('post-ping:', self)
+
+    def pingpong(self):
+        self.ping()
+        super().ping()
+        self.pong()
+        super().pong()
+        C.pong(self)
+
+d = D()
+d.pong()
+C.pong(d)
+
+print(D.__mro__)
+
+d.ping()
+
+d.pingpong()
