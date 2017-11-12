0111488df64e15dd2c8e6d4cd9947df419b61d7f
xushubo <tmac523@163.com>
2017-11-12 17:30:13 +0800

    updated 2017-11-12

--- 13_18_bingoaddable.py
+++ 13_18_bingoaddable.py
@@ -1,5 +1,6 @@
 import itertools
-from tombola import tombola
+import random
+from tombola import Tombola
 
 class BingoCage(Tombola):
 
@@ -20,3 +21,41 @@ class BingoCage(Tombola):
 
     def __call__(self):
         self.pick()
+
+class AddableBingoCage(BingoCage):
+    def __add__(self, other):
+        if isinstance(other, Tombola):
+            return AddableBingoCage(self.inspect() + other.inspect())
+        else:
+            return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other, Tombola):
+            other_iterable = other.inspect()
+        else:
+            try:
+                other_iterable = iter(other)
+            except TypeError:
+                self_cls = type(self).__name__
+                msg = 'right operand in += must be {!r} or an iterable'
+                raise TypeError(msg.format(self_cls))
+        self.load(other_iterable)
+        return self
+
+
+vowels = 'AEIOU'
+globe = AddableBingoCage(vowels)
+print(globe.inspect())
+print(globe.pick() in vowels)
+print(len(globe.inspect()))
+globe2 = AddableBingoCage('XYZ')
+globe3 = globe + globe2
+print(len(globe3.inspect()))
+globe_orig = globe
+print(len(globe_orig.inspect()))
+globe += globe2
+print(len(globe.inspect()))
+globe += ['M', 'N']
+print(len(globe.inspect()))
+print(globe is globe_orig)
+globe += 1
