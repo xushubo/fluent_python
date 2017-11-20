a41fad4b4852a778a0ad470bb206800436bb5a11
xushubo <tmac523@163.com>
2017-11-20 18:44:09 +0800

    updated 2017-11-20

---
+++ 14_14_example.py
@@ -0,0 +1,13 @@
+def vowel(c):
+    return c.lower() in 'aeiou'
+
+print(list(filter(vowel, 'Aardvark')))
+
+import itertools
+print(list(itertools.filterfalse(vowel, 'Aardvark')))
+print(list(itertools.dropwhile(vowel, 'Aardvark')))
+print(list(itertools.takewhile(vowel, 'Aardvark')))
+print(list(itertools.compress('Aardvark', (1, 0, 1, 1, 0, 1))))
+print(list(itertools.islice('Aardvark', 4)))
+print(list(itertools.islice('Aardvark', 4, 7)))
+print(list(itertools.islice('Aardvark', 1, 7, 2)))
