656ca787939decd53dc247d20f9298c1c526f970
xushubo <tmac523@163.com>
2017-11-24 17:20:31 +0800

    updated 2017-11-24

---
+++ 14_21_itertool_groupby.py
@@ -0,0 +1,23 @@
+import itertools
+print(list(itertools.groupby('LLLLAAGGG')))
+
+for char, group in itertools.groupby('LLLLAAGGG'):
+    print(char, '->', list(group))
+
+animals = ['duck', 'eagle', 'rat', 'giraffe', 'bear',
+            'bat', 'dolphin', 'shark', 'lion']
+
+for length, group in itertools.groupby(animals, len):
+    print(length, '->', list(group))
+    
+animals.sort(key=len)
+print(animals)
+
+for length, group in itertools.groupby(animals, len):
+    print(length, '->', list(group))
+
+for length, group in itertools.groupby(animals, lambda a: a[0]):
+    print(length, '->', list(group))
+
+for length, group in itertools.groupby(reversed(animals), len):
+    print(length, '->', list(group))
