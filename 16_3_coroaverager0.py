16f4dfd6445195d3c1e3128e45a896de9c49b09e
xushubo <tmac523@163.com>
2017-12-03 15:42:59 +0800

    updated 2017-12-3

---
+++ 16_3_coroaverager0.py
@@ -0,0 +1,15 @@
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield average
+        total += term
+        count += 1
+        average = total/count
+
+coro_avg = averager()
+next(coro_avg)
+print(coro_avg.send(10))
+print(coro_avg.send(30))
+print(coro_avg.send(5))
