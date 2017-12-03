ac76ed9437bd6f85190aa5d9be24e150f0227292
xushubo <tmac523@163.com>
2017-12-03 15:59:01 +0800

    updated 2017-12-3

---
+++ 16_6_coroaverager1.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+def coroutine(func):
+    """装饰器：向前执行到第一个`yield`表达式，预激`func`"""
+    @wraps(func)
+    def primer(*args, **kwargs):
+        gen = func(*args, **kwargs)
+        next(gen)
+        return gen
+    return primer
+
+@coroutine
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield average
+        total += term
+        count += 1
+        average = total/count
+
+coro_avg = averager()
+from inspect import getgeneratorstate
+print(getgeneratorstate(coro_avg))
+print(coro_avg.send(10))
+print(coro_avg.send(30))
+print(coro_avg.send(5))
