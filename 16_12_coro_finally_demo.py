aa92bc1f9f70c4a98c779c5c02d1e413f5cf97e4
xushubo <tmac523@163.com>
2017-12-18 10:19:46 +0800

    updated 2017-12-18

---
+++ 16_12_coro_finally_demo.py
@@ -0,0 +1,15 @@
+class DemoException(Exception):
+    """为这次演示定义的异常类型。 """
+
+def demo_exc_handling():
+    print('-> coroutine started')
+    try:
+        while True:
+            try:
+                x = yield
+            except DemoException:
+                print('*** DemoException handled. Continuing...')
+            else:
+                print('-> coroutine received: {!r}'.format(x))
+    finally:
+        print('-> coroutine ending')
