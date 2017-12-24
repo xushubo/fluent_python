2ad8f1039c2b9215f3538f0fd1867156f80a0dfd
xushubo <tmac523@163.com>
2017-12-24 11:37:37 +0800

    updated 2017-12-24

---
+++ 16_13_coroaverager2.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+Result = namedtuple('Result', 'count average')
+
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield
+        if term is None:
+            break
+        total += term
+        count += 1
+        average = total/count
+    return Result(count, average)
+
+coro_avg = averager()
+next(coro_avg)
+coro_avg.send(10)
+coro_avg.send(30)
+coro_avg.send(6.5)
+try:
+    coro_avg.send(None)
+except StopIteration as exc:
+    result = exc.value
+print(result)
