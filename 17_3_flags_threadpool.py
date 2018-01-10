9397a4d86081cd3882a2f99d99755a3bedf122d5
xushubo <tmac523@163.com>
2018-01-10 14:30:37 +0800

    updated 2018-1-10

---
+++ 17_3_flags_threadpool.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, 'wb') as fp:
+        fp.write(img)
+
+def get_flag(cc):
+    url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+def show(text):
+    pirnt(text, end=' ')
+    sys.stdout.flush()
+
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() = t0
+    msg = '\n{} flags downloaded in {:.2f}s'
+    print(msg.format(count, elapsed))
+
+MAX_WORKERS = 20
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+def download_many(cc_list):
+    workers = min(MAX_WORKERS, len(cc_list))
+    with futures.ThreadPoolExecutor(workers) as executor:
+        res = executor.map(download_one, sorted(cc_list))
+    return len(list(res))
+
+if __name__ == '__main__':
+    main(download_many)
